4a0fac37d71bd34aab6a13172f5a67cbb8bfcc77
Alexey Kovalev <41exey.kov41ev@gmail.com>
2021-03-04 09:54:13 +0300

    добавлено логгирование на сервер

--- echo_server/solution/with_logging/server.py
+++ echo_server/solution/with_logging/server.py
@@ -1,3 +1,17 @@
 import socket
 import logging
+from echo_server.solution.config import HOST, PORT
 
+logging.basicConfig(format="%(asctime)s.%(msecs)03d\t%(message)s", datefmt="%H:%M:%S", level=logging.INFO)
+logger = logging.getLogger("tcpserver")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    logger.info("Server is running")
+    s.listen(1)
+    logger.info("Server is listening")
+    conn, addr = s.accept()
+    with conn:
+        logger.info(f"Connected by {addr[0]}:{addr[1]}")
+        while data := conn.recv(1024):
+            conn.send(data)
